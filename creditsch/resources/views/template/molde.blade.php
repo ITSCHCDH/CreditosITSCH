e599ae99216bd102c9dbc7cd0abb5a74fd28788d
Oscar <osel0810029@gmail.com>
2017-10-04 21:25:18 -0500

    Plantilla
    Se creo la plantilla de molde para el sistema

--- creditsch/resources/views/template/molde.blade.php
+++ creditsch/resources/views/template/molde.blade.php
@@ -1,1 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Mi template</title>
+    <!-- Bootstrap core CSS-->
+    <link href="{{asset('plugins/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('plugins/vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="{{asset('plugins/css/sb-admin.css')}}" rel="stylesheet">
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+<!-- Navigation-->
+@include('template.partes.menu')
+
+<div class="content-wrapper">
+    <div class="container-fluid">
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index.html">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Blank Page</li>
+        </ol>
+        <div class="row">
+            <div class="col-12">
+                <h1>Blank</h1>
+                <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
+            </div>
+        </div>
+    </div>
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <footer class="sticky-footer">
+        <div class="container">
+            <div class="text-center">
+                <small>Copyright © Your Website 2017</small>
+            </div>
+        </div>
+    </footer>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('plugins/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('plugins/vendor/popper/popper.min.js')}}"></script>
+    <script src="{{asset('plugins/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('plugins/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <!-- Custom scripts for all pages-->
+    <script src=""></script>
+    <script src="{{asset('plugins/js/sb-admin.min.js')}}"></script>
+
+    <script src="{{asset('plugins/js/jquery-3.2.1.js')}}"></script>
+    <script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
+
+</div>
+</body>
+
+</html>
