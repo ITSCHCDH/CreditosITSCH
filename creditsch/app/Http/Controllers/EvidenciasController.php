1a676671740f68a0802395a14bbbcc201e9394db
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2018-04-30 15:37:22 -0500

    xxxx
    xxxxxxx

--- creditsch/app/Http/Controllers/EvidenciasController.php
+++ creditsch/app/Http/Controllers/EvidenciasController.php
@@ -8,6 +8,7 @@ use App\User;
 use App\Actividad;
 use App\Http\Requests\EvidenciasRequest;
 use Laracasts\Flash\Flash; //Es el paquete para poder usar los mensajes de alerta tipo bootstrap
+use Illuminate\Support\Facades\DB;
 
 class EvidenciasController extends Controller
 {
@@ -32,12 +33,14 @@ class EvidenciasController extends Controller
         //$evidencia->user_id=\Auth::user()->id;//Obtiene el id del usuario que esta logueado
         //Ponemos el codigo de la vista que se llamara para las altas de los creditos
         $usuarios=User::orderBy('name','asc')->pluck('name','id');//Traemos todas las categorias que existen en la bd
+
         $actividad=Actividad::orderBy('nombre','asc')->pluck('nombre','id');//Traemos todas las actividades
         /**********************************************************************************/
         $responsables=User::select('name','id')->orderBy('name')->pluck('name','id');
         return view('admin.evidencias.create')
             ->with('usuarios',$usuarios)
             ->with('actividad',$actividad)->with('responsables',$responsables);
+
     }
 
     /**
@@ -49,7 +52,20 @@ class EvidenciasController extends Controller
     public function store(EvidenciasRequest $request)
     {
         //Guarda la evidencia
+<<<<<<< HEAD
+    //    dd($request);
+=======
         //dd($request);
+        $id_actividad_evidencia_consulta = DB::table('actividad_evidencia')->where([
+            ['actividad_id','=',$request->id_asig_actividades],
+            ['user_id','=',$request->responsable],
+        ])->get();
+        if($id_actividad_evidencia_consulta->count()==0){
+            Flash::error('Error');
+            return back()->withInput();
+        }
+        //dd($id_actitidad_evidencia_consulta);
+>>>>>>> 5f889afd896f2facd157f22df3336d8a6c222eaf
         //Manipulacion de imagenes
         if($request->file('image'))//Validamos si existe una imagen
         {
@@ -62,13 +78,22 @@ class EvidenciasController extends Controller
             //Guardamos la imagen en la carpeta creada en la ruta que marcamos anteriormente
             $file->move($path,$name);
         }
-
+        //dd($id_actividad_evidencia_consulta);
+        $id_actividad_evidencia=-1;
+        foreach ($id_actividad_evidencia_consulta as $key) {
+            $id_actividad_evidencia=$key->id;
+            break;
+        }
+        ///dd($id_actividad_evidencia);
+        
         $evidencia=new Evidencia($request->all()); //Obtiene todos los datos de la evidencia de la vista create
+        $evidencia->id_asig_actividades=$id_actividad_evidencia;
         $evidencia->nom_imagen=$name;//Obtiene el nombre de la imagen para guardarlo en la bd
+
         $evidencia->save();//Guarda la evidencia en su tabla
 
         Flash::success('El articulo '.$evidencia->nombre.' se guardo con exito');
-        return redirect()->route('evidencias.index');
+        return redirect()->route('participantes.index');
     }
 
     /**
