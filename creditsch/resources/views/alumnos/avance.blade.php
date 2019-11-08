af44a032b59fe7f29fa35380beb5320f9ead5824
Oscar Delgado <osel0810029@gmail.com>
2019-11-08 15:00:20 -0600

    Tabla alumnos
    Se corrigio la tabla de  avance de los alumnos para que sea responsiva

--- creditsch/resources/views/alumnos/avance.blade.php
+++ creditsch/resources/views/alumnos/avance.blade.php
@@ -9,8 +9,8 @@
 @section('contenido')
 	
 	<a href="{{ route('alumnos.actividades') }}" class="btn btn-primary">Actividades</a>
-	<div style="width: 100%; margin-top:10px;" class="table-responsive">
-		<table class="table table-striped">
+	<div class="table-responsive">
+		<table class="table">
 			<thead>
 				<th colspan="2">Información del alumno</th>
 			</thead>
@@ -38,58 +38,87 @@
 			</tbody>
 		</table>
 	</div>
-
-	<table class="table table-striped" style="margin: 30px auto 50px auto; width:60%;">
+<div class="table-responsive">
+	<table class="table">
 	    <!-- instancia al archivo table, dentro de este mismo direcctorio -->
-	<thead>
-		<th>Credito</th>
-		<th>Actividades</th>
-		<th>Porcentaje</th>
-	</thead>
-	<tbody>
-		@if($alumno_data!=null)
-			@php
-				$temp_porcentaje = 0;
-				$temp_suma = 0;
-				$y=0;
-			@endphp
-			@for ($x = 0; $x < count($creditos); $x++)
-				
-				@if ( $y < count($alumno_data) && $avance)
-					@if ($alumno_data[$y]->nombre_credito==$creditos[$x]->nombre)
-						@php
-							$temp_porcentaje = ($alumno_data[$y]->por_credito >100)?100:$alumno_data[$y]->por_credito;
-							$temp_suma += (int)$temp_porcentaje;
-						@endphp
-						<tr>
-							<td colspan="3" style="text-align:left; padding: 0;">
-								<div style="position: relative;">
-									<div style="background: #27ce1e; padding: 5; width: {{$temp_porcentaje}}%; height: 35px;max-width: 100%";>
-										<div style="position: absolute;">
-											<strong>{{ $alumno_data[$y]->nombre_credito }}</strong>
+		<thead class="thead-dark">
+			<th>Credito</th>
+			<th>Actividades</th>
+			<th>Porcentaje</th>
+		</thead>
+		<tbody>
+			@if($alumno_data!=null)
+				@php
+					$temp_porcentaje = 0;
+					$temp_suma = 0;
+					$y=0;
+				@endphp
+				@for ($x = 0; $x < count($creditos); $x++)
+					
+					@if ( $y < count($alumno_data) && $avance)
+						@if ($alumno_data[$y]->nombre_credito==$creditos[$x]->nombre)
+							@php
+								$temp_porcentaje = ($alumno_data[$y]->por_credito >100)?100:$alumno_data[$y]->por_credito;
+								$temp_suma += (int)$temp_porcentaje;
+							@endphp
+							<tr>
+								<td colspan="3" style="text-align:left; padding: 0;">
+									<div style="position: relative;">
+										<div style="background: #27ce1e; padding: 5; width: {{$temp_porcentaje}}%; height: 35px;max-width: 100%";>
+											<div style="position: absolute;">
+												<strong>{{ $alumno_data[$y]->nombre_credito }}</strong>
+											</div>
+											
 										</div>
-										
 									</div>
-								</div>
-							</td>
-						</tr>
-						@for (; $y < count($alumno_data) && $alumno_data[$y]->nombre_credito==$creditos[$x]->nombre; $y++)
+								</td>
+							</tr>
+							@for (; $y < count($alumno_data) && $alumno_data[$y]->nombre_credito==$creditos[$x]->nombre; $y++)
+								<tr>
+									<td></td>
+									<td>{{ $alumno_data[$y]->nombre_actividad }}</td>
+									<td>{{ $alumno_data[$y]->por_cred_actividad.'%' }}</td>
+								</tr>
+							@endfor
 							<tr>
 								<td></td>
-								<td>{{ $alumno_data[$y]->nombre_actividad }}</td>
-								<td>{{ $alumno_data[$y]->por_cred_actividad.'%' }}</td>
+								<td></td>
+								<td ><strong>Total</strong></td>
 							</tr>
-						@endfor
-						<tr>
-							<td></td>
-							<td></td>
-							<td ><strong>Total</strong></td>
-						</tr>
-						<tr>
-							<td></td>
-							<td></td>
-							<td>{{ $temp_porcentaje.'%' }}</td>
-						</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>{{ $temp_porcentaje.'%' }}</td>
+							</tr>
+						@else
+							<tr>
+								<td colspan="3" style="text-align:left; padding: 0;">
+									<div style="position: relative;">
+										<div style="background: #27ce1e; padding: 5; width: 0%;height: 35px;max-width: 100%";>
+											<div style="position: absolute;">
+												<strong >{{ $creditos[$x]->nombre }}</strong>
+											</div>
+											
+										</div>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>No actividades registradas</td>
+								<td>0%</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td ><strong>Total</strong></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>0%</td>
+							</tr>
+						@endif
 					@else
 						<tr>
 							<td colspan="3" style="text-align:left; padding: 0;">
@@ -119,53 +148,25 @@
 							<td>0%</td>
 						</tr>
 					@endif
-				@else
-					<tr>
-						<td colspan="3" style="text-align:left; padding: 0;">
-							<div style="position: relative;">
-								<div style="background: #27ce1e; padding: 5; width: 0%;height: 35px;max-width: 100%";>
-									<div style="position: absolute;">
-										<strong >{{ $creditos[$x]->nombre }}</strong>
-									</div>
-									
-								</div>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>No actividades registradas</td>
-						<td>0%</td>
-					</tr>
-					<tr>
+				@endfor
+				@php
+					if($temp_suma>500)$temp_suma=500;
+					$temp_suma/=100;
+				@endphp
+				<tr>
 						<td></td>
 						<td></td>
-						<td ><strong>Total</strong></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td></td>
-						<td>0%</td>
-					</tr>
-				@endif
-			@endfor
-			@php
-				if($temp_suma>500)$temp_suma=500;
-				$temp_suma/=100;
-			@endphp
-			<tr>
+						<td><strong>Total de creditos</strong></td>
+				</tr>
+				<tr>
 					<td></td>
 					<td></td>
-					<td><strong>Total de creditos</strong></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-				<td>{{ $temp_suma.' creditos' }}</td>
-			</tr>
-		@endif
-	</tbody>
+					<td>{{ $temp_suma.' creditos' }}</td>
+				</tr>
+			@endif
+		</tbody>
 	</table>
+</div>
 	@if($liberado)
 		<a href="{{ route('alumnos.constancias_imprimir') }}" class="btn btn-primary">Imprimir constancia</a>
 	@endif
