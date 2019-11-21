b18502e11ca869f0bcb2a073bc5b89f4b569e8e6
Oscar Delgado <osel0810029@gmail.com>
2019-11-20 20:57:25 -0600

    Mejoras de menu
    Se afinaron detalles de apariencia en el menu

--- creditsch/resources/views/template/partes/menu.blade.php
+++ creditsch/resources/views/template/partes/menu.blade.php
@@ -1,5 +1,5 @@
 <!-- Menu del sistema -->
-<div class="container">
+<div>
     <p>
         
         @if(Auth::guard('web')->check())
@@ -12,8 +12,8 @@
             <div class="card ">
                 <div class="card-header">
                     <a class="card-link colMen" href="/alumnos/home">
-                        <i class="fa fa-home" style="font-size:20px"></i>
-                        <span >Inicio</span>
+                        <i class="fa fa-home" style="font-size:15px"></i>
+                        <span> &nbsp;&nbsp;Inicio</span>
                     </a>
                 </div>     
             </div>  
@@ -21,8 +21,8 @@
             <div class="card">
                 <div class="card-header">
                     <a class="card-link colMen" href="/home">
-                        <i class="fa fa-home" style="font-size:20px"></i>
-                        <span >Inicio</span>
+                        <i class="fa fa-home" style="font-size:15px"></i>
+                        <span >&nbsp;&nbsp;Inicio</span>
                     </a>
                 </div>     
             </div>    
@@ -32,8 +32,8 @@
             <div class="card">
                 <div class="card-header">    
                     <a class="card-link colMen" href="{{route('alumnos.index')}}">
-                        <i class="fa fa-graduation-cap" style="font-size:20px"></i>
-                        <span >Alumnos</span>
+                        <i class="fa fa-graduation-cap" style="font-size:15px"></i>
+                        <span >&nbsp;&nbsp;Alumnos</span>
                     </a>
                 </div>     
             </div>                
@@ -42,8 +42,8 @@
             <div class="card">
                 <div class="card-header">          
                     <a class="card-link colMen" href="{{route('creditos.index')}}">
-                        <i class="fa fa-fw fa-table" style="font-size:20px"></i>
-                        <span >Créditos</span>
+                        <i class="fa fa-fw fa-table" style="font-size:15px"></i>
+                        <span >&nbsp;&nbsp;Créditos</span>
                     </a> 
                 </div>     
             </div>          
@@ -52,8 +52,8 @@
                 <div class="card">
                     <div class="card-header">
                         <a class="card-link colMen" data-toggle="collapse" href="#collapseOne">
-                            <i class="fa fa-fw fa-list-ul" style="font-size:20px"></i>
-                            <span >Actividades</span>
+                            <i class="fa fa-fw fa-list-ul" style="font-size:15px"></i>
+                            <span >&nbsp;&nbsp;Actividades&nbsp;&nbsp;</span>
                             <i class='fas fa-angle-down'></i>                        
                         </a>
                     </div>                   
@@ -61,24 +61,24 @@
                             @if (Auth::User()->can('VIP')|| Auth::User()->can('VER_ACTIVIDAD') || Auth::User()->can('VIP_ACTIVIDAD') || Auth::User()->can('VIP_SOLO_LECTURA'))
                                 <div class="card-body">
                                     <a class="colMen" href="{{route('actividades.index')}}">
-                                        <i class="fa fa-futbol-o" style="font-size:15px"></i>
-                                        <span >Administrar actividades</span>
+                                        <i class="fa fa-futbol-o" style="font-size:10px"></i>
+                                        <span >&nbsp;&nbsp;Administrar actividades</span>
                                     </a>
                                 </div>
                             @endif
                             @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA','VIP_EVIDENCIA','VER_PARTICIPANTES']))
                                 <div class="card-body">
                                     <a class="colMen" href="{{route('participantes.index')}}">
-                                        <i class="fa fa-users" style="font-size:15px"></i>
-                                        <span >Agregar participantes</span>
+                                        <i class="fa fa-users" style="font-size:10px"></i>
+                                        <span >&nbsp;&nbsp;Agregar participantes</span>
                                     </a>
                                 </div>
                             @endif
                             @if (Auth::User()->can('VIP') || Auth::User()->can('VIP_EVIDENCIA') || Auth::User()->can('VER_EVIDENCIA') || Auth::User()->can('VIP_SOLO_LECTURA'))
                                 <div class="card-body">
                                     <a class="colMen" href="{{route('evidencias.index')}}">
-                                        <i class="fa fa-camera" style="font-size:15px"></i>
-                                        <span >Evidencia</span>
+                                        <i class="fa fa-camera" style="font-size:10px"></i>
+                                        <span >&nbsp;&nbsp;Evidencia</span>
                                     </a>
                                 </div>
                              @endif
@@ -89,8 +89,8 @@
                 <div class="card">
                     <div class="card-header">
                         <a class="collapsed card-link colMen" data-toggle="collapse" href="#collapseTwo">
-                            <i class="fa fa-pie-chart" style="font-size:20px"></i>
-                            <span >Avances</span>
+                            <i class="fa fa-pie-chart" style="font-size:15px"></i>
+                            <span >&nbsp;&nbsp;Avances&nbsp;&nbsp;</span>
                             <i class='fas fa-angle-down'></i>
                         </a>
                     </div>
@@ -98,32 +98,32 @@
                         @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA','VER_AVANCE_ALUMNO']))
                             <div class="card-body">
                                 <a class="colMen" href="{{ route('verifica_evidencia.avance_alumno') }}">
-                                    <i class="fa fa-bar-chart-o" style="font-size:15px"></i>
-                                    <span >Avance</span>
+                                    <i class="fa fa-bar-chart-o" style="font-size:10px"></i>
+                                    <span >&nbsp;&nbsp;Avance</span>
                                 </a>
                             </div>
                         @endif
                         @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA','VIP_EVIDENCIA','VERIFICAR_EVIDENCIA']))
                             <div class="card-body">
                                 <a class="colMen" href="{{ route('verifica_evidencia.index') }}">
-                                    <i class="fa fa-calendar-check-o" style="font-size:15px"></i>
-                                    <span >Verificar evidencia</span>
+                                    <i class="fa fa-calendar-check-o" style="font-size:10px"></i>
+                                    <span >&nbsp;&nbsp;Verificar evidencia</span>
                                 </a>
                             </div>
                         @endif
                         @if (Auth::User()->hasAnyPermission('VIP','VIP_CONSTANCIAS','MODIFICAR_CONSTANCIAS_CARRERA'))
                             <div class="card-body">
                                 <a class="colMen" href="{{ route('constancias.index') }}">
-                                    <i class="fa fa-print" style="font-size:15px"></i>
-                                    <span >Constancias</span>
+                                    <i class="fa fa-print" style="font-size:10px"></i>
+                                    <span >&nbsp;&nbsp;Constancias</span>
                                 </a>
                             </div>
                         @endif
                         @if (Auth::User()->hasAnyPermission(['VER_REPORTES_CARRERA','VIP_REPORTES','VIP_SOLO_LECTURA','VIP']))
                             <div class="card-body">
                                 <a class="colMen" href="{{ route('verifica_evidencia.reportes') }}">
-                                    <i class="fa fa-file-pdf-o" style="font-size:15px"></i>
-                                    <span >Reportes</span>
+                                    <i class="fa fa-file-pdf-o" style="font-size:10px"></i>
+                                    <span >&nbsp;&nbsp;Reportes</span>
                                 </a>
                             </div>
                         @endif
@@ -133,55 +133,55 @@
             <div class="card">
                 <div class="card-header">
                     <a class="collapsed card-link colMen" data-toggle="collapse" href="#collapseThree">
-                        <i class="fa fa-gear" style="font-size:20px"></i>
-                        <span >Administración</span>
+                        <i class="fa fa-gear" style="font-size:15px"></i>
+                        <span >&nbsp;&nbsp;Administración&nbsp;&nbsp;</span>
                         <i class='fas fa-angle-down'></i>                        
                     </a>
                 </div>
                 <div id="collapseThree" class="collapse" data-parent="#accordion">
                     <div class="card-body">
                         <a class="colMen" href="{{ route('perfil.index') }}">
-                            <i class="fa fa-user" style="font-size:15px"></i>
-                            <span >Mi perfil</span>
+                            <i class="fa fa-user" style="font-size:10px"></i>
+                            <span >&nbsp;&nbsp;Mi perfil</span>
                         </a>
                     </div>
                     @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA','VER_USUARIOS']))
                         <div class="card-body">
                             <a class="colMen" href="{{ route('usuarios.index') }}">
-                                <i class="fa fa-users" style="font-size:15px"></i>
-                                <span >Usuarios</span>
+                                <i class="fa fa-users" style="font-size:10px"></i>
+                                <span >&nbsp;&nbsp;Usuarios</span>
                             </a>
                         </div>
                     @endif
                     @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA','VER_ROLES']))
                         <div class="card-body">
                             <a class="colMen" href="{{ route('roles.index') }}">
-                                <i class="fa fa-address-card" style="font-size:15px"></i>
-                                <span >Roles</span>
+                                <i class="fa fa-address-card" style="font-size:10px"></i>
+                                <span >&nbsp;&nbsp;Roles</span>
                             </a>
                         </div>
                     @endif
                     @if(Auth::User()->hasAnyPermission('VIP','VER_AREAS','VIP_SOLO_LECTURA'))
                         <div class="card-body">
                             <a class="colMen" href="{{ route('areas.inicio') }}">
-                                <i class="fa fa-map-marker" style="font-size:15px"></i>
-                                <span >Areas</span>
+                                <i class="fa fa-map-marker" style="font-size:10px"></i>
+                                <span >&nbsp;&nbsp;Areas</span>
                             </a>
                         </div>
                     @endif
                     @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA']))
                         <div class="card-body">
                             <a class="colMen" href="{{ route('ImportExcel.index') }}">
-                                <i class="glyphicon glyphicon-import" style="font-size:15px"></i>
-                                <span >Modificar password alumnos</span>
+                                <i class="glyphicon glyphicon-import" style="font-size:10px"></i>
+                                <span >&nbsp;&nbsp;Modificar password alumnos</span>
                             </a>
                         </div>
                     @endif   
                     @if (Auth::User()->hasAnyPermission(['VIP','VIP_SOLO_LECTURA']))
                         <div class="card-body">
                             <a class="colMen" href="{{ route('ImportExcel.altaAlumnos') }}">
-                                <i class="glyphicon glyphicon-circle-arrow-up" style="font-size:15px"></i>
-                                <span >Insertar alumnos desde excel</span>
+                                <i class="glyphicon glyphicon-circle-arrow-up" style="font-size:10px"></i>
+                                <span >&nbsp;&nbsp;Insertar alumnos desde excel</span>
                             </a>
                         </div>
                     @endif
@@ -190,8 +190,8 @@
             <div class="card">
                 <div class="card-header"> 
                     <a  class="colMen" href="{{ route('mensajes.index') }}">
-                        <i class='far fa-envelope' style="font-size:20px"></i>
-                        <span >Mensajes</span>
+                        <i class='far fa-envelope' style="font-size:15px"></i>
+                        <span >&nbsp;&nbsp;Mensajes</span>
                     </a>
                 </div>     
             </div> 
@@ -199,8 +199,8 @@
                 <div class="card">
                     <div class="card-header">       
                         <a class="colMen"  onclick="event.preventDefault(); document.getElementById('logout-form2').submit();">
-                            <i class="material-icons" style="font-size:20px">exit_to_app</i>
-                            <span >Cerrar sesión </span>
+                            <i class="material-icons" style="font-size:15px">exit_to_app</i>
+                            <span >&nbsp;&nbsp;Cerrar sesión </span>
                         </a>
                         <!-- Creamos el formulario en orden para poder cerrar sesion -->
                         <form id="logout-form2" action="{{ route('logout')}}" method="POST" style="display: none;">
