cbeaa3d5820c0b68943b82ccc79c84b88d9e7809
Kioselsa <osel0810029@gmail.com>
2022-11-30 15:21:04 -0600

    mensaje

--- creditsch/public/Caracterizacion/conexion/conex_ms_sql.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$server_mssql="172.16.0.10";
-	$bd_mssql="CONTROL_ESCOLAR";
-	$user_mssql="sista";
-	$pwd_mssql="Sta_2019$";
-?>
--- creditsch/public/Caracterizacion/conexion/conex_trayec_mysql.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-
-	$hostname2="localhost";
-	$database2="Trayectoria_escolar";
-	$username2="trayacademic";
-	$password2="v8fnFeMtNFN&";
-
-?>
