d80298e4274f57a3466af45ab7ec4d37514bdbdb
kioselsa <30932646+kioselsa@users.noreply.github.com>
2024-06-05 12:16:32 -0600

    Actualización 05062024

--- resources/views/sta/tutorias/libTutPDF.blade.php
+++ resources/views/sta/tutorias/libTutPDF.blade.php
@@ -34,32 +34,32 @@
          <table>
             <tbody>
                 <tr>
-                    <td style="width: 150px"></td>
-                    <td><img src="{{ asset('images/itsch.jpg') }}" alt="ISTCH" style="max-width: 40px;"></td>
+                    <td style="width: 50px;"></td>
+                    <td style="width: 100px;"><img src="{{ asset('images/itsch.jpg') }}" alt="ISTCH" style="max-width: 40px;"></td>
                     <td><h6>INSTITUTO TECNOLÓGICO SUPERIOR DE CIUDAD HIDALGO</h6></td>
-                    <td></td>
+                    <td style="width: 100px; text-align:right;"><img src="{{ asset('images/LogoTecNM.png') }}" alt="TECNM" style="max-width: 80px;"></td>
                 </tr>
             </tbody>
          </table>
     </head>
     <h5>A QUIEN CORRESPONDA: </h5>
-    <p>LOS QUE SUSCRIBEN, LA LPE. MARIA DE LOURDES SÁNCHEZ MORA. JEFA DEL DEPARTAMENTO DE TUTORIAS Y SERVICIOS PSICOPEDAGÓGICOS Y LA MTRA. MARÍA GRISELDA CORREA GARCIA, ENCARGADA DE LA SUBDIRECCIÓN ACADÉMICA DEL INSTITUTO TECNOLÓGICO NACIONAL DE MÉXICO CAMPUS CIUDAD HIDALGO: </p>
+    <p style="line-height:3;">LOS QUE SUSCRIBEN, LA MTRA. MARIA DE LOURDES SÁNCHEZ MORA. JEFA DEL DEPARTAMENTO DE TUTORIAS Y SERVICIOS PSICOPEDAGÓGICOS Y EL ISC. OSCAR DELGADO CAMACHO, ENCARGADO DE LA SUBDIRECCIÓN ACADÉMICA DEL INSTITUTO TECNOLÓGICO NACIONAL DE MÉXICO CAMPUS CIUDAD HIDALGO: </p>
     <div style="text-align: center;">
         <b>HACEN CONSTAR</b>
     </div>
-    <p>
+    <p style="line-height:3;">
         QUE <b>{{ strtoupper($data->name) }}</b> CONCLUYÓ SATISFACTORIAMENTE CON EL PROGRAMA INSTITUCIONAL DE TUTORÍAS EN EL SEMESTRE <b>{{  strtoupper($data->gtu_Semestre) }} {{ $data->gtu_Año }}</b> 
-        EN EL INSTITUTO TECNOLÓGICO NACIONAL DE MÉXICO CAMPUS CIUDAD HIDALGO CUMPLIENDO CON LA MODALIDAD DE  <b>{{strtoupper($data->gtu_Tipo) }}</b>
+        EN EL INSTITUTO TECNOLÓGICO SUPERIOR DE CIUDAD HIDALGO, CUMPLIENDO CON LA MODALIDAD DE  <b>{{strtoupper($data->gtu_Tipo) }}</b>
         EN EL GRUPO <b>{{ strtoupper($data->gpo_Nombre) }}</b> DE LA CARRERA DE <b>{{ strtoupper($data->car_Nombre) }}</b>. 
     </p>
 
-    <p>
-        A SOLICITUD DEL INTERESADO SE EMITE LA PRESENTE PARA LOS FINES QUE EL INTERESADO JUZGUE CONVENIENTES EN CIUDAD HIDALGO, MICHOACÁN A
+    <p style="line-height:3;">
+        A SOLICITUD DEL INTERESADO(A), SE EMITE LA PRESENTE PARA LOS FINES QUE ÉL(ELLA) JUZGUE CONVENIENTES. EN CIUDAD HIDALGO, MICHOACÁN A
         {{ strtoupper(\Carbon\Carbon::now()->locale('es')->isoFormat('D [de] MMMM YYYY')) }}        
     </p>
     
     
-    <div style="height: 200px"></div>
+    <div style="height: 100px"></div>
 
     <table>
         <tbody>
@@ -72,7 +72,7 @@
                     <br>
                     <br>
                     <br>
-                    <b><small>LPE. MARÍA DE LOURDES SÁNCHEZ MORA</small></b>
+                    <b><small>MTRA. MARÍA DE LOURDES SÁNCHEZ MORA</small></b>
                     <p><small>JEFA DEL DEPARTAMENTO DE TUTORIAS Y SERV. PSICO.</small></p>
                     <p><small>INSTITUTO TECNOLÓGICO SUPERIOR DE CIUDAD HIDALGO</small></p>
                 </td>
@@ -85,15 +85,13 @@
                     <br>
                     <br>
                     <br>
-                    <b><small>MTRA. MARÍA GRISELDA CORREA GARCÍA</small></b>
-                    <p><small>ENCARGADA DE LA SUBDIRECCIÓN ACADÉMICA</small></p>
+                    <b><small>ISC. OSCAR DELGADO CAMACHO</small></b>
+                    <p><small>ENCARGADO DE LA SUBDIRECCIÓN ACADÉMICA</small></p>
                     <p><small>INSTITUTO TECNOLÓGICO SUPERIOR DE CIUDAD HIDALGO</small></p>
                 </td>               
             </tr>
         </tbody>
-    </table>
-   
-    <br>
+    </table> 
     
     <p><small>C.C.P Para el archivo</small></p>
 
